02a45e65c7d5428b99a77822b09477fa0b11e645
enoxie <admin@enesbilgi.com>
2022-07-29 18:37:50 +0300

    v1.0.0

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import AppContext from "../provider/appContext";
+
+function Footer() {
+  const {
+    app: { isDark },
+  } = useContext(AppContext);
+  const anchorStyle = {
+    color: "#C6C6C6",
+  };
+  return (
+    <>
+      <footer className="footer">
+        <a
+          href="https://github.com/enoxie/weather-app"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-brands fa-github"></i> <span>GitHub</span>
+        </a>
+        <a
+          href="https://github.com/enoxie/weather-app"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-solid fa-code-fork"></i> <span>Fork</span>
+        </a>
+        <a
+          href="https://github.com/enoxie/weather-app"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-solid fa-star"></i> <span>Star</span>
+        </a>
+        
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
